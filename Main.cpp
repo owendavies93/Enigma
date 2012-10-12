4af48591e50fbffd673d2d6aa4d6377aad07b2a7
Owen <tod11@doc.ic.ac.uk>
2012-10-12 23:10:45 +0100

    Added input read-in and paramter error checking

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #include "enigma.h"
 
@@ -11,29 +14,42 @@ int main(int argc, char **argv) {
 	//Parse .pb file to give to plugboard (via Enigma)
     int plugs[26];
     int size = 0;
-    ifstream plugboardStream(argv[argc - 1], ifstream::in);
 
-    if (plugboardStream.fail()) {
+    try {
+        ifstream plugboardStream(argv[argc - 1], ifstream::in);
+        //plugboardStream.exceptions(ifstream::failbit);
 
+	    while (plugboardStream >> plugs[size]) {
+	   	   ++size;
+	    }
+    } catch (ifstream::failure fail) {
+        cerr << "Error - " << argv[argc - 1]  << " could not be read." << endl
+             << strerror(errno) << endl;
+        _exit(1);
     }
-
-	while (plugboardStream >> plugs[size]) {
-		++size;
-	}
 
 	//Create machine, with plugboard
     Enigma machine(plugs, size);
-    vector<ifstream*> streams;
 
     //Create rotors if necessary
     for (int i = 1; i < argc - 1; i++) {
-        ifstream rotorStream(argv[i], ifstream::in);
-        streams.push_back(&rotorStream);
+        try {
+            ifstream rotorStream(argv[i], ifstream::in);
+            rotorStream.exceptions(ifstream::failbit|ifstream::badbit);
 
-        ifstream& streamRef = rotorStream;
-        machine.addRotor(streamRef, i);
+            ifstream& streamRef = rotorStream;
+            machine.addRotor(streamRef, i);
+        } catch (ifstream::failure fail) {
+            cerr << "Error - " << argv[i]  << " could not be read." << endl
+                 << strerror(errno) << endl;
+            _exit(1);
+        }
     }
 
-    string inputString = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "Enter string to encode: ";
+    string inputString;
+    cin >> inputString;
     machine.encrypt(inputString);
+
+   
 }
